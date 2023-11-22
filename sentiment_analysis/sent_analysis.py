8bdde799a57d19316b744b2804ee54630d06d9bd
Eske Haack <eskehaack@gmail.com>
2023-11-22 13:02:07 +0100

    added sentiment analysis as module

---
+++ sentiment_analysis/sent_analysis.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import nltk
+from tqdm import tqdm
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+nltk.download('stopwords')
+da_stop = set(nltk.corpus.stopwords.words('danish'))
+
+sent_df = pd.read_csv("sentiment_analysis/2_headword_headword_polarity.csv", header=None)[[0, 4]]
+sentiment_dict = {row[0]: row[4] for _, row in sent_df.iterrows()}
+
+def get_tfs(text):
+    return dict(nltk.probability.FreqDist(([lemmatizer.lemmatize(w) for word in text if (w:=word.lower()).isalpha()])))
+
+# Use this function to find sentiments of node attributes
+def get_sents(node_att):
+    post_sents = dict()
+    for post_id, text in tqdm(node_att.items()):
+        tf = get_tfs(nltk.word_tokenize(text))
+        if (denom := np.sum([tf[word] for word in tf.keys() if word in list(sentiment_dict.keys())])) != 0 :
+            post_sents[post_id] = (
+                np.sum([sentiment_dict[word] * tf[word] for word in tf.keys() if word in list(sentiment_dict.keys())]) / 
+                denom
+            )
+        else:
+            post_sents[post_id] = 0
+    
+    return post_sents
